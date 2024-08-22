835d0c091b013eb5af1538e5485db096ca465fba
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-08-22 12:12:19 +0530

    fix: bg & FloatingShape

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,39 @@
+import FloatingShape from "./components/FloatingShape";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="relative flex items-center justify-center min-h-screen overflow-hidden bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </div>
   );
 }
 
